a8653e810e6d9eaa31c99104945b25e01078dd3e
Efim Sivolap <efim.sivolap@gmail.com>
2022-07-06 21:13:22 +0500

    change design, color, fonts

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var blocks = document.getElementsByClassName("container__item");
-var container = document.getElementsByClassName("container");
-var hs = new HorizontalScroll.default({
-  blocks: blocks,
-  container: container,
-  isAnimated: true,
-  springEffect: 0.8,
-});
